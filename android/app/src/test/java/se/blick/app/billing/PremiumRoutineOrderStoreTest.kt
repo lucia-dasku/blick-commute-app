3c21d14f8b15661d4a0a456258059395b77bca1f
Lucia <liucija.daskute@gmail.com>
2026-08-23 17:58:05 +0200

    Pin routine order test SDK

--- android/app/src/test/java/se/blick/app/billing/PremiumRoutineOrderStoreTest.kt
+++ android/app/src/test/java/se/blick/app/billing/PremiumRoutineOrderStoreTest.kt
@@ -8,8 +8,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34], application = android.app.Application::class)
 class PremiumRoutineOrderStoreTest {
     private val context: Context = RuntimeEnvironment.getApplication()
 
